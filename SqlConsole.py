ec3d4481bd30fb4d355a618e3fd8d9eaed51eb38
tongelja <tongelja@hotmail.com>
2018-01-05 15:17:13 -0600

    Updates to clean up code

--- SqlConsole.py
+++ SqlConsole.py
@@ -1,6 +1,6 @@
 
 
-import sys, getpass, re, traceback
+import sys, getpass, re, traceback, os
 
 try:
     import pypyodbc, cx_Oracle
@@ -19,6 +19,9 @@ class db:
         self.results = []
 
         self.connected_user = ''
+
+        self.keyword_values = [ 'EXIT', 'SAVE' , 'DESC', 'DESCRIBE' ]
+
 
     def mssqlOpen(self, server=None, user=None, password=None, port=1433):
         self.connection_info['type'] = 'MSSql'
@@ -77,32 +80,36 @@ class db:
         sql_stmt = ''
         text = ''
         print('\n')
-        prompt = self.connection_info['host'].upper() + '/' + self.connection_info['database'].lower() 
+        prompt = self.connection_info['host'].split('.')[0].upper() + '/' + self.connection_info['database'].lower() 
         print('-'* (len(prompt) + 5))
         print('| ' + prompt)
         print('-'* (len(prompt) + 5))
 
-        text = input('\nSQL> ').upper()
-        match =  re.search('^\s*(\w+)(\s|$)+', text, re.I)
-        keyword_input = match.group(1)
-        keyword_values = [ 'SAVE' , 'DESC', 'DESCRIBE' ]
+        text = input('\nSQL> ')
 
         while True:
-            if text.strip() == '/':
-                break
-            elif keyword_input in keyword_values:
+
+            ##
+            ## Keyword matching in this section to allow keywords to end with no ';'
+            ## This functionality replicates other SQL consoles
+            ##
+
+            match =  re.search('^\s*(\w+)(\s|$)+', text, re.I)
+            keyword_input = match.group(1).upper()
+  
+            if keyword_input in self.keyword_values:
                 sql_stmt = text
+                break
+            elif text.strip() == '/' or text.strip == ';':
                 break
             elif re.search('.*;$', text):
                 sql_stmt += ' ' + str(text)
                 break
-            elif text.strip() == 'EXIT':
-                return 'EXIT'
             else:
                 sql_stmt += ' ' + str(text)
             text = input('      ')
 
-        sql_stmt = re.sub('(\n|;$)', ' ', sql_stmt).strip()
+        sql_stmt = re.sub('(\n|;$|\/$)', ' ', sql_stmt).strip()
         return sql_stmt
 
     def __write_table__(self):
@@ -143,6 +150,12 @@ class db:
         self.curs = self.conn.cursor()
 
         output      = self.curs.execute(sql_stmt)
+
+        if hasattr(output, 'description'):
+            description = output.description
+        else:
+            description = 'None'
+ 
         description = output.description
 
         self.results01['data'] = [ r for r in output ]
@@ -154,18 +167,34 @@ class db:
 
     def __parse_oracle_sql_query__(self, sql_stmt):
         self.curs = self.conn.cursor()
+        self.results01 = {}
 
         output = self.curs.execute(sql_stmt)
-        description = output.description
 
-        self.results01['data'] = [ r for r in output ]
-        self.results01['desc'] = description
-        self.results = [ r for r in self.results01['data'] ]
+        if hasattr(output, 'description'):
+            description = output.description
+        else:
+            description = 'None'
+
+        if output is None:
+            self.results01['data'] = [['Complete']]
+            self.results01['desc'] = [['Call Results']]
+        else:
+            self.results01['data'] = [ r for r in output ]
+            self.results01['desc'] = description
+            self.results = [ r for r in self.results01['data'] ]
 
         self.__write_table__()
 
 
-    def __save_results__(self, result_name):
+    def __save_results__(self, sql_stmt):
+
+        match = re.search('save (\w+)', sql_stmt, re.I)
+        if match:
+           result_name = match.group(1).lower()
+        else:
+            result_name = 'default_save'
+
         self.saved_results[result_name] = self.results01['data']
 
     def __parse_sql__(self, sql_stmt):
@@ -173,49 +202,66 @@ class db:
                 print('\nERROR: No SQL Connection')
                 sys.exit(2)
 
+        save_query = {'MSSql'  : 'self.__parse_mssql_save_query__',
+                      'Oracle' : 'self.__parse_oracle_save_query__' }
+
         desc_query = {'MSSql'  : 'self.__parse_mssql_desc_query__',
                       'Oracle' : 'self.__parse_oracle_desc_query__' }
 
         exec_query = {'MSSql'  : 'self.__parse_mssql_sql_query__',
                       'Oracle' : 'self.__parse_oracle_sql_query__' }
 
-        if re.search('^\s*SAVE', sql_stmt.upper(), re.I):
-            action = 'SAVE'
-        elif re.search('^\s*DESC[CRIBE]?', sql_stmt.upper(), re.I):
-            action = 'DESCRIBE'
-        else:
-            action = 'QUERY'
 
-        if action == 'SAVE':
-            match = re.search('save (\w+)', sql_stmt, re.I)
-            if match:
-                saved_result = match.group(1).lower()
-                self.__save_results__(saved_result) 
+        match =  re.search('^\s*(\w+)(\s|$)+', sql_stmt, re.I)
+        keyword_input = match.group(1).upper()
 
-        elif action == 'DESCRIBE':
-            match = re.search('desc(ribe)? (.*)', sql_stmt, re.I)
-            if match:
-                fullpath_object = match.group(2).lower()
-            
-                if len(fullpath_object.split('.')) == 2:
-                    db_owner  = fullpath_object.split('.')[0].upper()
-                    db_object = fullpath_object.split('.')[1].upper()
-                elif len(object.split('.')) == 1:
-                    db_object = fullpath_object
-                else:
-                    db_owner=''
-                    db_object = ''
-            eval( desc_query[ self.connection_info['type'] ])(db_owner, db_object)  
+        if keyword_input == 'SAVE':
+            self.__save_results__(sql_stmt) 
+
+        elif keyword_input == 'DESCRIBE' or keyword_input == 'DESC':
+            eval( desc_query[ self.connection_info['type'] ])(sql_stmt)  
 
         else:
             eval( exec_query[ self.connection_info['type'] ])(sql_stmt)  
 
-    def __parse_mssql_desc_query__(self, db_owner, db_object):
-        sql_smt = ''
+
+    def __parse_mssql_desc_query__(self, sql_stmt):
+
+        match = re.search('desc(ribe)? (.*)', sql_stmt, re.I)
+        if match:
+            fullpath_object = match.group(2)
+
+        if len(fullpath_object.split('.')) == 2:
+            db_owner  = fullpath_object.split('.')[0].upper()
+            db_object = fullpath_object.split('.')[1].upper()
+        elif len(object.split('.')) == 1:
+            db_object = fullpath_object
+        else:
+            db_owner=''
+            db_object = ''
+
+        sql_stmt = 'exec sp_columns ' + db_object
         self.__parse_mssql_sql_query__(sql_stmt)
 
-    def __parse_oracle_desc_query__(self, db_owner, db_object):
-        sql_stmt = "select rpad(column_name, 40, ' ' ) ||  data_type || '(' ||  data_length || ')'  as " + db_object + " from dba_tab_columns where owner = '" + db_owner + "' and table_name = '" + db_object + "'"
+    def __parse_oracle_desc_query__(self, sql_stmt):
+
+
+        match = re.search('desc(ribe)? (.*)', sql_stmt, re.I)
+        if match:
+            fullpath_object = match.group(2)
+
+        if len(fullpath_object.split('.')) == 2:
+            db_owner  = fullpath_object.split('.')[0].upper()
+            db_object = fullpath_object.split('.')[1].upper()
+        elif len(object.split('.')) == 1:
+            db_object = fullpath_object
+        else:
+            db_owner=''
+            db_object = ''
+
+        sql_stmt = "select rpad(column_name, 40, ' ' ) ||  data_type || '(' ||  data_length || ')'  as " + db_object + \
+                   " from dba_tab_columns where owner = '" + db_owner + "' and table_name = '" + db_object + "'"
+
         self.__parse_oracle_sql_query__(sql_stmt)
  
 
@@ -237,6 +283,11 @@ class db:
                     try:
                         self.__parse_sql__(sql_stmt)
                     except Exception as err:
+                        exc_type, exc_obj, exc_tb = sys.exc_info()
+                        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
                         print('\nERROR parsing SQL\n')
                         print(str(err))
+                        #print(exc_type, fname, exc_tb.tb_lineno)
+                        #print('Error on line {}'.format(sys.exc_info()[-1].tb_lineno), type(e).__name__, e)
+
 
