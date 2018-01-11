a6943824e18c46bc272b77c4428306c1174513f4
tongelja <tongelja@hotmail.com>
2018-01-11 12:18:59 -0600

    updates

--- SqlConsole.py
+++ SqlConsole.py
@@ -9,7 +9,7 @@ except:
     sys.exit(2)
 
 
-class db:
+class SqlDb:
     def __init__(self):
         self.connection_info = {}
         self.conn = None
@@ -23,7 +23,155 @@ class db:
         self.keyword_values = [ 'EXIT', 'SAVE' , 'DESC', 'DESCRIBE', '@', 'RUN' ]
 
 
-    def mssqlOpen(self, server=None, user=None, password=None, port=1433):
+    def cmd(self, sql_stmt=None):
+        if self.conn is None:
+            print('No Connection')
+            sys.exit(2)
+
+        if sql_stmt != None:
+            self.parse_sql(sql_stmt)
+        else:
+            while True:
+                sql_stmt = self.sql_prompt()
+                if sql_stmt.upper() == 'EXIT':
+                    break
+                elif sql_stmt.upper() == '':
+                    pass
+                else:
+                    try:
+                        self.parse_sql(sql_stmt)
+                    except Exception as err:
+                        print('\nERROR parsing SQL\n')
+                        exc_type, exc_value, exc_traceback = sys.exc_info()
+                        traceback.print_tb(exc_traceback, limit=1, file=sys.stdout)
+                        traceback.print_exception(exc_type, exc_value, exc_traceback, limit=4, file=sys.stdout)
+
+
+    def parse_sql(self, sql_stmt):
+        if self.conn == 'None':
+                print('\nERROR: No SQL Connection')
+                sys.exit(2)
+
+        match =  re.search('^\s*(.+?)(\s|$)+', sql_stmt, re.I)
+        keyword_input = match.group(1).upper()
+        match = re.search('(.)', keyword_input)
+        keyword_first_char = match.group(1)
+
+        if keyword_input == 'SAVE':
+            self.save_results(sql_stmt)
+
+        elif keyword_input == 'DESCRIBE' or keyword_input == 'DESC':
+            self.desc(sql_stmt)
+
+        elif keyword_input == 'RUN' or keyword_first_char == '@':
+            self.script(sql_stmt)
+
+        else:
+            self.query(sql_stmt)
+
+
+
+    def write_table(self):
+        if len(self.results01['data']) == 0:
+            print('No data found')
+            return
+
+        row_length = len(self.results01['data'][0])
+        max_length = [1] * row_length
+
+        i = self.results01['desc']
+        for j in range(len(i)):
+            if max_length[j] < len(str(i[j][0])):
+                max_length[j] = len(str(i[j][0]))
+
+        for i in self.results01['data']:
+            for j in range(len(i)):
+                if max_length[j] < len(str(i[j])):
+                    max_length[j] = len(str(i[j]))
+
+
+        i = self.results01['desc']
+        print('\n', end='')
+        for j in range(len(i)):
+            length = max_length[j]
+            print('{:<{width}}'.format(str(i[j][0]).upper(), width=length), end='  ')
+
+        print('\n', end='')
+        for j in range(len(i)):
+            length = max_length[j]
+            print('{:<{width}}'.format('-'*length, width=length), end='  ')
+
+        for i in self.results01['data']:
+            print('\n', end='')
+            for j in range(len(i)):
+                length = max_length[j]
+                print('{:<{width}}'.format(str(i[j]), width=length), end='  ')
+        print('\n', end='')
+
+
+    def save_results(self, sql_stmt):
+
+        match = re.search('save (\w+)', sql_stmt, re.I)
+        if match:
+           result_name = match.group(1).lower()
+        else:
+            result_name = 'default_save'
+
+        self.saved_results[result_name] = self.results01['data']
+
+
+    def sql_prompt(self):
+        sql_stmt = ''
+        text = ''
+        print('\n')
+        prompt = self.connection_info['host'].split('.')[0].upper() + '/' + self.connection_info['database'].lower()
+        print('-'* (len(prompt) + 5))
+        print('| ' + prompt)
+        print('-'* (len(prompt) + 5))
+
+        text = input('\nSQL> ')
+
+        while True:
+
+            ##
+            ## Keyword matching in this section to allow keywords to end with no ';'
+            ## This functionality replicates other SQL consoles
+            ##
+
+            match =  re.search('^\s*(\w+)(\s|$)+', text, re.I)
+            if match:
+                keyword_input = match.group(1).upper()
+            else:
+                keyword_input = ''
+
+            match =  re.search('^\s*(.)', text, re.I)
+            if match:
+                keyword_first_char_input = match.group(1).upper()
+            else:
+                keyword_first_char_input = ''
+
+            if keyword_input in self.keyword_values or keyword_first_char_input in self.keyword_values:
+                sql_stmt = text
+                break
+            elif text.strip() == '':
+                sql_stmt = text
+                break
+            elif text.strip() == '/' or text.strip == ';':
+                break
+            elif re.search('.*;$', text):
+                sql_stmt += ' ' + str(text)
+                break
+            else:
+                sql_stmt += ' ' + str(text)
+            text = input('      ')
+
+        sql_stmt = re.sub('(\n|;$|\/$)', ' ', sql_stmt).strip()
+        return sql_stmt
+
+
+
+class Mssql(SqlDb):
+    def open(self, server=None, user=None, password=None, port=1433):
         self.connection_info['type'] = 'MSSql'
         if server is None:
             server = input('Server ==> ')
@@ -43,8 +191,76 @@ class db:
         results = conn.cursor().execute('select db_name()').fetchone()
         self.connection_info['database'] = results[0]
 
+    def query(self, sql_stmt):
+        self.curs = self.conn.cursor()
 
-    def oracleOpen(self, server=None, database=None, user=None, password=None):
+        output      = self.curs.execute(sql_stmt)
+
+        if hasattr(output, 'description'):
+            description = output.description
+        else:
+            description = 'None'
+
+        description = output.description
+
+        self.results01['data'] = [ r for r in output ]
+        self.results01['desc'] = description
+        self.results = self.results01['data']
+
+        self.write_table()
+
+    def script(self, sql_stmt):
+
+        match = re.search('(@|run\s+)?(.*)', sql_stmt, re.I)
+
+        if match:
+            script_name = match.group(2)
+            script = open(script_name).read().strip()
+        script = re.sub('\n', ' ', script)
+        sql_stmt = re.search('(select .*)(\;$|\/$)', script, re.I).group(1)
+        #sql_stmt = re.sub('(;|\/)', ' ', sql_stmt)
+
+        match = re.search('(&\w+)', sql_stmt, re.I)
+        if match:
+            input_variables=match.groups()
+        else:
+            input_variables=[]
+
+        var_translation = {}
+        for i in range(len(input_variables)):
+            var_substitute = input('Value for ' + input_variables[i] + ':  ')
+            var_translation[input_variables[i]] = var_substitute
+            sql_stmt = re.sub(input_variables[i], var_substitute, sql_stmt)
+
+
+        print('File: ' + script_name)
+        print('SQL: ' + sql_stmt)
+
+        self.query(sql_stmt)
+
+
+    def desc(self, sql_stmt):
+
+        match = re.search('desc(ribe)? (.*)', sql_stmt, re.I)
+        if match:
+            fullpath_object = match.group(2)
+
+        if len(fullpath_object.split('.')) == 2:
+            db_owner  = fullpath_object.split('.')[0].upper()
+            db_object = fullpath_object.split('.')[1].upper()
+        elif len(object.split('.')) == 1:
+            db_object = fullpath_object
+        else:
+            db_owner=''
+            db_object = ''
+
+        sql_stmt = 'exec sp_columns ' + db_object
+        self.query(sql_stmt)
+
+
+
+class Oracle(SqlDb):
+    def open(self, server=None, database=None, user=None, password=None):
         self.connection_info['type'] = 'Oracle'
         if server is None:
             server = input('Server ==> ')
@@ -91,114 +307,7 @@ class db:
 
 
 
-    def __sql_prompt__(self):
-        sql_stmt = ''
-        text = ''
-        print('\n')
-        prompt = self.connection_info['host'].split('.')[0].upper() + '/' + self.connection_info['database'].lower() 
-        print('-'* (len(prompt) + 5))
-        print('| ' + prompt)
-        print('-'* (len(prompt) + 5))
-
-        text = input('\nSQL> ')
-
-        while True:
-
-            ##
-            ## Keyword matching in this section to allow keywords to end with no ';'
-            ## This functionality replicates other SQL consoles
-            ##
-
-            match =  re.search('^\s*(\w+)(\s|$)+', text, re.I)
-            if match:
-                keyword_input = match.group(1).upper()
-            else:
-                keyword_input = ''
-
-            match =  re.search('^\s*(.)', text, re.I)
-            if match:
-                keyword_first_char_input = match.group(1).upper()
-            else:
-                keyword_first_char_input = ''
- 
-            if keyword_input in self.keyword_values or keyword_first_char_input in self.keyword_values:
-                sql_stmt = text
-                break
-            elif text.strip() == '':
-                sql_stmt = text
-                break
-            elif text.strip() == '/' or text.strip == ';':
-                break
-            elif re.search('.*;$', text):
-                sql_stmt += ' ' + str(text)
-                break
-            else:
-                sql_stmt += ' ' + str(text)
-            text = input('      ')
-
-        sql_stmt = re.sub('(\n|;$|\/$)', ' ', sql_stmt).strip()
-        return sql_stmt
-
-    def __write_table__(self):
-        if len(self.results01['data']) == 0:
-            print('No data found')
-            return
-
-        row_length = len(self.results01['data'][0])
-        max_length = [1] * row_length
-
-        i = self.results01['desc']
-        for j in range(len(i)):
-            if max_length[j] < len(str(i[j][0])):
-                max_length[j] = len(str(i[j][0]))
-
-        for i in self.results01['data']:
-            for j in range(len(i)):
-                if max_length[j] < len(str(i[j])):
-                    max_length[j] = len(str(i[j]))
-
-
-        i = self.results01['desc']
-        print('\n', end='')
-        for j in range(len(i)):
-            length = max_length[j]
-            print('{:<{width}}'.format(str(i[j][0]).upper(), width=length), end='  ')
-
-        print('\n', end='')
-        for j in range(len(i)):
-            length = max_length[j]
-            print('{:<{width}}'.format('-'*length, width=length), end='  ')
-            
-        for i in self.results01['data']:
-            print('\n', end='')
-            for j in range(len(i)):
-                length = max_length[j]
-                print('{:<{width}}'.format(str(i[j]), width=length), end='  ')
-        print('\n', end='')
-
-       
-
-
-    def __parse_mssql_sql_query__(self, sql_stmt):
-        self.curs = self.conn.cursor()
-
-        output      = self.curs.execute(sql_stmt)
-
-        if hasattr(output, 'description'):
-            description = output.description
-        else:
-            description = 'None'
- 
-        description = output.description
-
-        self.results01['data'] = [ r for r in output ]
-        self.results01['desc'] = description
-        self.results = self.results01['data']
-
-        self.__write_table__()
-
-
-    def __parse_oracle_sql_query__(self, sql_stmt):
+    def query(self, sql_stmt):
         self.curs = self.conn.cursor()
         self.results01 = {}
 
@@ -218,7 +327,7 @@ class db:
                 self.results01['desc'] = description
                 self.results = [ r for r in self.results01['data'] ]
 
-            self.__write_table__()
+            self.write_table()
 
         except Exception as err:
             exc_type, exc_obj, exc_tb = sys.exc_info()
@@ -226,50 +335,7 @@ class db:
             print('\nERROR parsing SQL: ' + sql_stmt +'\n')
 
 
-        
-
-    def __save_results__(self, sql_stmt):
-
-        match = re.search('save (\w+)', sql_stmt, re.I)
-        if match:
-           result_name = match.group(1).lower()
-        else:
-            result_name = 'default_save'
-
-        self.saved_results[result_name] = self.results01['data']
-
-    def __parse_sql__(self, sql_stmt):
-        if self.conn == 'None':
-                print('\nERROR: No SQL Connection')
-                sys.exit(2)
-
-        desc_query   = {'MSSql'  : 'self.__parse_mssql_desc___', 'Oracle' : 'self.__parse_oracle_desc__' }
-
-        exec_query   = {'MSSql'  : 'self.__parse_mssql_sql_query__',  'Oracle' : 'self.__parse_oracle_sql_query__' }
-
-        script_query = {'MSSql'  : 'self.__parse_mssql_script__',  'Oracle' : 'self.__parse_oracle_script__' }
-
-
-        match =  re.search('^\s*(.+?)(\s|$)+', sql_stmt, re.I)
-        keyword_input = match.group(1).upper()
-        match = re.search('(.)', keyword_input)
-        keyword_first_char = match.group(1)
-      
-        if keyword_input == 'SAVE':
-            self.__save_results__(sql_stmt) 
-
-        elif keyword_input == 'DESCRIBE' or keyword_input == 'DESC':
-            eval( desc_query[ self.connection_info['type'] ])(sql_stmt)  
-
-        elif keyword_input == 'RUN' or keyword_first_char == '@':
-            eval( script_query[ self.connection_info['type'] ])(sql_stmt)  
-
-        else:
-            eval( exec_query[ self.connection_info['type'] ])(sql_stmt)  
-
-
-
-    def __parse_mssql_script__(self, sql_stmt):
+    def script(self, sql_stmt):
 
         match = re.search('(@|run\s+)?(.*)', sql_stmt, re.I)
 
@@ -296,58 +362,9 @@ class db:
         print('File: ' + script_name)
         print('SQL: ' + sql_stmt)
 
-        self.__parse_mssql_sql_query__(sql_stmt)
+        self.query(sql_stmt)
 
-
-    def __parse_oracle_script__(self, sql_stmt):
-
-        match = re.search('(@|run\s+)?(.*)', sql_stmt, re.I)
-
-        if match:
-            script_name = match.group(2)
-            script = open(script_name).read().strip()
-        script = re.sub('\n', ' ', script)
-        sql_stmt = re.search('(select .*)(\;$|\/$)', script, re.I).group(1)
-        #sql_stmt = re.sub('(;|\/)', ' ', sql_stmt)
-
-        match = re.search('(&\w+)', sql_stmt, re.I)
-        if match:
-            input_variables=match.groups()
-        else:
-            input_variables=[]
-
-        var_translation = {}
-        for i in range(len(input_variables)):
-            var_substitute = input('Value for ' + input_variables[i] + ':  ')
-            var_translation[input_variables[i]] = var_substitute
-            sql_stmt = re.sub(input_variables[i], var_substitute, sql_stmt) 
-        
-        
-        print('File: ' + script_name)
-        print('SQL: ' + sql_stmt)
-
-        self.__parse_oracle_sql_query__(sql_stmt)
-
-    def __parse_mssql_desc__(self, sql_stmt):
-
-        match = re.search('desc(ribe)? (.*)', sql_stmt, re.I)
-        if match:
-            fullpath_object = match.group(2)
-
-        if len(fullpath_object.split('.')) == 2:
-            db_owner  = fullpath_object.split('.')[0].upper()
-            db_object = fullpath_object.split('.')[1].upper()
-        elif len(object.split('.')) == 1:
-            db_object = fullpath_object
-        else:
-            db_owner=''
-            db_object = ''
-
-        sql_stmt = 'exec sp_columns ' + db_object
-        self.__parse_mssql_sql_query__(sql_stmt)
-
-
-    def __parse_oracle_desc__(self, sql_stmt):
+    def desc(self, sql_stmt):
 
         match = re.search('desc(ribe)? (.*)', sql_stmt, re.I)
         if match:
@@ -367,31 +384,9 @@ class db:
         sql_stmt = "select rpad(column_name, 40, ' ' ) ||  data_type || '(' ||  data_length || ')'  as " + db_object + \
                    " from dba_tab_columns where owner = '" + db_owner + "' and table_name = '" + db_object + "'"
 
-        self.__parse_oracle_sql_query__(sql_stmt)
+        self.query(sql_stmt)
  
 
-    def cmd(self, sql_stmt=None):
-        if self.conn is None:
-            print('No Connection')
-            sys.exit(2)
-
-        if sql_stmt != None:
-            self.__parse_sql__(sql_stmt)
-        else:
-            while True:
-                sql_stmt = self.__sql_prompt__()
-                if sql_stmt.upper() == 'EXIT':
-                    break
-                elif sql_stmt.upper() == '':
-                    pass
-                else:
-                    try:
-                        self.__parse_sql__(sql_stmt)
-                    except Exception as err:
-                        print('\nERROR parsing SQL\n')
-                        exc_type, exc_value, exc_traceback = sys.exc_info()
-                        traceback.print_tb(exc_traceback, limit=1, file=sys.stdout)
-                        traceback.print_exception(exc_type, exc_value, exc_traceback, limit=4, file=sys.stdout)
 
 
 
