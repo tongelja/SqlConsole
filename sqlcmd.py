60f86b728a6a8a8952e9b7e96de77f3ef867caba
tongelja <tongelja@hotmail.com>
2018-03-15 13:39:59 -0500

    Removed sqlcmd.py

--- sqlcmd.py
+++
@@ -1,8 +0,0 @@
-import SqlConsole, getpass
-o = SqlConsole.SqlConsole()
-host       = input('Host: ')
-db         = input('Service: ')
-user       = input('User: ')
-password   = getpass.getpass('Password: ')
-o.mssqlOpen(host, user, password)
-o.cmd()
